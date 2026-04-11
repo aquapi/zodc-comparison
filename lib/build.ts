1539631ae62eb2184f7637a550d5ae99ee010b7e
aquapi <aquaplmc@gmail.com>
2026-04-11 21:10:04 +0700

    mmm

--- lib/build.ts
+++ lib/build.ts
@@ -10,5 +10,5 @@ export const BUNDLE_OPTIONS: BuildOptions = {
     },
     target: 'esnext',
   },
-  external: [/^bun:/, 'bun', 'ata-validator'],
+  external: [/^bun:/, 'bun', 'ata-validator', 'mitata'],
 }
